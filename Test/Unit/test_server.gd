680453a2e4d17de129387c9dc9b5f26c9dd83776
MagicalRon <my@email.com>
2023-07-22 12:03:13 -0400

    Server Tests: Mock Connection

--- Test/Unit/test_server.gd
+++ Test/Unit/test_server.gd
@@ -3,10 +3,7 @@ extends GutTest
 class TestServerStandalone:
 	extends GutTest
 	var serverObj = load( "res://Network.gd" )
-	var clientObj = load( "res://Test/MockClient.gd")
-
 	var server = null
-	var client = null
 
 	func before_each():
 		server = autofree( serverObj.new() )
@@ -33,3 +30,33 @@ class TestServerStandalone:
 	func test_establish_server_on_null_peer():
 		server.peer = null
 		assert_false( server.establish_server(), "Establish server with null peer should be false")
+
+class TestServerConnections:
+	extends GutTest
+	var serverObj = load( "res://Network.gd" )
+	var clientObj = load( "res://Test/MockClient.gd")
+	var server = null
+	var client = null
+
+	const TIMEOUT = 1
+
+	func before_each():
+		server = autofree( serverObj.new() )
+		self.add_child( server )
+		# Move the scene tree multiplayer default root to the server instance
+		get_tree().set_multiplayer( get_tree().get_multiplayer(), server.get_path() )
+		assert_true( server.establish_server(), "server_establish() return false in init" )
+		# Watch signals emitted from server & multiplayer
+		watch_signals( server )
+		watch_signals( multiplayer )
+
+	func test_client_connection():
+		client = autofree( clientObj.new() )
+		self.add_child( client )
+		var client_id = client.mock_client.get_unique_id()
+		await yield_to( server, "PlayerConnected", TIMEOUT, "Timed out waiting for client connection" )
+		assert_signal_emitted_with_parameters( server, "PlayerConnected", [ client_id ] )
+
+		self.remove_child( client )
+		await yield_to( server, "PlayerDisconnected", TIMEOUT, "Timed out waiting for client disconnection" )
+		assert_signal_emitted_with_parameters( server, "PlayerDisconnected", [ client_id ] )
