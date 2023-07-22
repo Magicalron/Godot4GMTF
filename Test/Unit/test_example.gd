3719a081551909107484f0be931b35d56ce9963d
MagicalRon <my@email.com>
2023-07-22 14:04:43 -0400

    Example Tests: Example Pinger

--- Test/Unit/test_example.gd
+++ Test/Unit/test_example.gd
@@ -2,3 +2,53 @@ extends GutTest
 
 func test_init():
 	assert_true( true, "Something went dramatically wrong" )
+
+class MyObjectToTest:
+	extends Node
+
+	signal WasPinged
+
+	@rpc( "any_peer" )
+	func ping():
+		print( "{0} was pinged from {1}".format(
+			[multiplayer.get_unique_id(), multiplayer.get_remote_sender_id()]))
+		emit_signal( "WasPinged" )
+
+	@rpc( "authority")
+	func authoritative_ping():
+		print( "{0} was pinged from the server".format([multiplayer.get_unique_id()]))
+		emit_signal( "WasPinged" )
+
+class TestServerMyObjectToTest:
+	extends MockNetwork.Server
+
+	# This is the object that would appear in your servers scene tree
+	var serverTestObject = null
+	# This is the object that would appear in your clients scene tree
+	var clientTestObject = null
+
+	func before_each():
+		var nodes = add_test_node( MyObjectToTest, "test_object" )
+		serverTestObject = nodes[0]
+		clientTestObject = nodes[1]
+		watch_signals( serverTestObject )
+		watch_signals( clientTestObject )
+
+	func test_server_to_client_ping():
+		serverTestObject.rpc("ping")
+		await yield_to( clientTestObject, "WasPinged", TIMEOUT )
+		assert_signal_emitted( clientTestObject, "WasPinged" )
+
+	func test_client_to_server_ping():
+		clientTestObject.rpc("ping")
+		await yield_to( serverTestObject, "WasPinged", TIMEOUT )
+		assert_signal_emitted( serverTestObject, "WasPinged" )
+
+	func test_authority_ping():
+		serverTestObject.rpc("authoritative_ping")
+		await yield_to( clientTestObject, "WasPinged", TIMEOUT )
+		assert_signal_emitted( clientTestObject, "WasPinged" )
+
+		clientTestObject.rpc("authoritative_ping")
+		await yield_to( serverTestObject, "WasPinged", TIMEOUT )
+		assert_signal_not_emitted( serverTestObject, "WasPinged" )
