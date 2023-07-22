dd883891ccbe0f24bfc497cc19cc31b24d4cadcb
MagicalRon <my@email.com>
2023-07-22 13:31:44 -0400

    Client Tests: Mock Connection

--- Test/Unit/test_client.gd
+++ Test/Unit/test_client.gd
@@ -30,3 +30,33 @@ class TestClientStandalone:
 	func test_establish_client_on_null_peer():
 		client.peer = null
 		assert_false( client.establish_client(), "Establish client with null peer should be false")
+
+
+class TestClientConnections:
+	extends GutTest
+	var clientObj = load( "res://Network.gd" )
+	var serverObj = load( "res://Test/MockServer.gd")
+	var client = null
+	var server = null
+
+	const TIMEOUT = 1
+
+	func before_each():
+		client = autofree( clientObj.new() )
+		self.add_child( client )
+		# Move the scene tree multiplayer default root to the server instance
+		get_tree().set_multiplayer( get_tree().get_multiplayer(), client.get_path() )
+		assert_true( client.establish_client(), "server_establish() return false in init" )
+		# Watch signals emitted from client & multiplayer
+		watch_signals( client )
+		watch_signals( multiplayer )
+
+	func test_client_connection():
+		server = autofree( serverObj.new() )
+		self.add_child( server )
+		await yield_to( client, "PlayerConnected", TIMEOUT, "Timed out waiting for client connection" )
+		assert_signal_emitted( client, "PlayerConnected", [ client.multiplayer_id ] )
+
+		client.peer.close()
+		await yield_to( client, "PlayerDisconnected", TIMEOUT, "Timed out waiting for client disconnection" )
+		assert_signal_emitted_with_parameters( client, "PlayerDisconnected", [ client.multiplayer_id ] )
